049eed36d86d10ea862d8a9377111c5b53d6f8b3
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2018-06-04 06:33:21 +0200

    Shelves & Book components style

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,10 +4,10 @@ class Book extends React.Component {
     render() {
         const book = this.props.book;
         return (
-            <div>
+            <div className="book">
                 <img src={book.imageLinks.smallThumbnail} alt={book.title}/>
                 <p>{book.title}</p>
-                <p>{book.authors}</p>
+                <p className="author">{book.authors[0]}</p>
             </div>
         );
     }
